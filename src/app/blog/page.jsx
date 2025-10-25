4095a70ce9d60e26553936e2013f3ba36639e893
Ayaan <iamayaanalee@gmail.com>
2025-10-25 22:38:03 +0500

    Updatwe

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -112,7 +112,7 @@ export default async function Blogs({ searchParams: maybeSearchParams }) {
                 src={blog.coverImage}
                 alt={blog.title}
                 fill
-                className="object-cover group-hover:scale-105 transition-transform duration-500"
+                className="object-fill group-hover:scale-105 transition-transform duration-500"
               />
             </div>
 
